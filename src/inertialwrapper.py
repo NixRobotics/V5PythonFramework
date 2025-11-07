daa20a5e4957edb486c13c874350252c4db8de96
NixRobotics <144736633+NixRobotics@users.noreply.github.com>
2025-11-06 21:24:36 -0800

    initial for gyro

---
+++ src/inertialwrapper.py
@@ -0,0 +1,56 @@
+from vex import Inertial, RotationUnits
+
+class InertialWrapper(Inertial):
+
+    # @param port
+    # @param gyro_scale: GYRO_SCALE_FOR_FOR_READOUT
+    def __init__(self, port, gyro_scale):
+        super().__init__(port)
+        self.gyro_scale = gyro_scale
+
+    def rotation(self):
+        return super().rotation(RotationUnits.DEG) * self.gyro_scale
+
+    def heading(self):
+        return InertialWrapper.to_heading(self.rotation())
+
+    def angle(self):
+        return InertialWrapper.to_angle(self.rotation())
+    
+    def set_rotation(self, value, units=RotationUnits.DEG):
+        if units is not RotationUnits.DEG:
+            raise NotImplementedError("InertialWrapper.set_rotation(): Units must be in degrees")
+        rotation = value / self.gyro_scale
+        return super().set_rotation(rotation, RotationUnits.DEG)
+    
+    def set_heading(self, heading):
+        angle = InertialWrapper.to_angle(heading)
+        return self.set_rotation(angle, RotationUnits.DEG)
+
+    @staticmethod
+    def to_heading(rotation):
+        return rotation % 360.0
+
+    @staticmethod
+    def to_angle(rotation):
+        angle = rotation % 360.0
+        if (angle > 180.0): angle -= 360.0
+        return angle
+
+    def calc_angle_to_heading(self, heading):
+        current_heading = self.heading()
+        delta_heading = heading - current_heading
+        delta_angle = InertialWrapper.to_angle(delta_heading) / self.gyro_scale # divide: sic
+        return delta_angle
+
+    def calc_rotation_at_heading(self, heading):
+        current_rotation = self.rotation()
+        current_heading = InertialWrapper.to_heading(current_rotation)
+
+        delta_heading = heading - current_heading
+        delta_angle = InertialWrapper.to_angle(delta_heading)
+
+        new_rotation = current_rotation + delta_angle
+        new_rotation *= (1.0 / self.gyro_scale)
+
+        return new_rotation
--- src/main.py
+++ src/main.py
@@ -9,11 +9,49 @@
 
 # Library imports
 from vex import *
+from inertialwrapper import InertialWrapper
+
+GYRO_SCALE_FOR_READOUT = 361.0/360.0
+inertial = InertialWrapper(Ports.PORT5, GYRO_SCALE_FOR_READOUT)
 
 # Brain should be defined by default
 brain=Brain()
 
-brain.screen.print("Hello V5")
+ROBOT_INITIALIZED = False
 
+def pre_autonomous():
+    global ROBOT_INITIALIZED
 
-        
+    wait(0.1, SECONDS)
+
+    if inertial.installed():
+        inertial.calibrate()
+        while inertial.is_calibrating():
+            wait(50, MSEC)
+
+    ROBOT_INITIALIZED = True
+
+def autonomous():
+    while not ROBOT_INITIALIZED:
+        wait(10, MSEC)
+
+def user_control():
+    while not ROBOT_INITIALIZED:
+        wait(10, MSEC)
+
+    if not inertial.installed():
+        brain.screen.print("NO INERTAIL SENSOR")
+        while True:
+            wait(20, MSEC)
+
+    wait(1, SECONDS)
+    print(inertial.rotation())
+    inertial.set_heading(270.0)
+    print(inertial.rotation())
+
+    while True:
+        wait(10, MSEC)
+
+comp = Competition(user_control, autonomous)
+pre_autonomous()
+
