1c8efba0c6c15b2fe355116616c9b42c17048066
NixRobotics <144736633+NixRobotics@users.noreply.github.com>
2025-11-06 20:12:59 -0800

    initial

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Nick                                                         #
+# 	Created:      11/6/2025, 8:12:02 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
